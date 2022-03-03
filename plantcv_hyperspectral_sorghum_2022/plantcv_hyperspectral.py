1022e3c325c346b9cf28a3d0f422fbb95635dbde
micheal-whaley <44105295+micheal-whaley@users.noreply.github.com>
2022-03-03 15:15:47 -0600

    Updated plantcv_hyperspectral.py
    Script skips analysis if there is no obj_area

--- plantcv_hyperspectral_sorghum_2022/plantcv_hyperspectral.py
+++ plantcv_hyperspectral_sorghum_2022/plantcv_hyperspectral.py
@@ -129,137 +129,136 @@ def main():
                                                                   obj_hierarchy=obj_hierarchy,
                                                                   roi_type='partial')
 
-    obj, mask = pcv.object_composition(img, roi_objects, hierarchy3)
+    if obj_area > 0:
+        obj, mask = pcv.object_composition(img, roi_objects, hierarchy3)
 
     ############### Analysis ################
+    ## below 3 lines were commented out before
+    #    outfile=False
+    #    if args.writeimg == True:
+    #	outfile = args.outdir + "/" + filename
+
+        # Find shape properties, output shape image (optional)
+        shape_imgs = pcv.analyze_object(img, obj, mask)
+
+        # Shape properties relative to user boundary line (optional)
+        if "z1000" in filename:
+            boundary_img1 = pcv.analyze_bound_horizontal(img,obj,mask,1470)
+        elif "z2000" in filename:
+            boundary_img1 = pcv.analyze_bound_horizontal(img, obj, mask, 1680)
+        else:
+            boundary_img1 = pcv.analyze_bound_horizontal(img, obj, mask, 1370)
+
+        # Determine color properties: Histograms, Color Slices, output color analyzed histogram (optional)
+        color_histogram = pcv.analyze_color(img, kept_mask, 'all')
+
+        # Pseudocolor the grayscale image
+        pseudocolored_img = pcv.visualize.pseudocolor(gray_img=s, mask=kept_mask, cmap='jet')
+
+        nir_path = pcv.get_nir(path=path,filename=filename)
+        nir_img, path, img_filename = pcv.readimage(filename= nir_path, mode="native")
 
 
-## below 3 lines were commented out before
-#    outfile=False
-#    if args.writeimg == True:
-#	outfile = args.outdir + "/" + filename
+        # In[161]:
 
-    # Find shape properties, output shape image (optional)
-    shape_imgs = pcv.analyze_object(img, obj, mask)
+        if "z1000" in filename:
+            img_warped, mat = pcv.transform.warp(img=kept_mask,
+                    refimg=nir_img,
+                    pts = [(229, 241), (234, 1156), (1050, 1467), (2230, 241),
+                                                 (2227, 1171), (1411, 1461)],
+                    refpts = [(28, 72), (30, 332), (262,422), (600, 75),
+                                                        (594, 333), (364, 420)],
+                    method='lmeds')
 
-    # Shape properties relative to user boundary line (optional)
-    if "z1000" in filename:
-        boundary_img1 = pcv.analyze_bound_horizontal(img,obj,mask,1470)
-    elif "z2000" in filename:
-        boundary_img1 = pcv.analyze_bound_horizontal(img, obj, mask, 1680)
-    else:
-        boundary_img1 = pcv.analyze_bound_horizontal(img, obj, mask, 1370)
+            img_warped_original, mat = pcv.transform.warp(img=img,
+                    refimg=nir_img,
+                    pts = [(229, 241), (234, 1156), (1050, 1467), (2230, 241),
+                                                 (2227, 1171), (1411, 1461)],
+                    refpts = [(28, 72), (30, 332), (262,422), (600, 75),
+                                                        (594, 333), (364, 420)],
+                    method='lmeds')
 
-    # Determine color properties: Histograms, Color Slices, output color analyzed histogram (optional)
-    color_histogram = pcv.analyze_color(img, kept_mask, 'all')
+        elif "z2000" in filename:
+            img_warped, mat = pcv.transform.warp(img=kept_mask,
+                	refimg=nir_img,
+                	pts = [(1005, 1660), (1140, 1730), (1140, 1850), (1413, 1677),
+                                             (1389, 1734), (1389, 1854)],
+                	refpts = [(254, 200), (291, 214), (290, 245), (372, 200),
+                                                    (362, 214), (361, 247)],
+                	method='lmeds')
 
-    # Pseudocolor the grayscale image
-    pseudocolored_img = pcv.visualize.pseudocolor(gray_img=s, mask=kept_mask, cmap='jet')
+            img_warped_original, mat = pcv.transform.warp(img=img,
+                	refimg=nir_img,
+                	pts = [(1005, 1660), (1140, 1730), (1140, 1850), (1413, 1677),
+                                             (1389, 1734), (1389, 1854)],
+                	refpts = [(254, 200), (291, 214), (290, 245), (372, 200),
+                                                    (362, 214), (361, 247)],
+                	method='lmeds')
 
-    nir_path = pcv.get_nir(path=path,filename=filename)
-    nir_img, path, img_filename = pcv.readimage(filename= nir_path, mode="native")
+        else:
+            img_warped, mat = pcv.transform.warp(img=kept_mask,
+                	refimg=nir_img,
+                	pts = [(495, 435), (500, 1120), (510, 1795), (1990, 440),
+                                             (1985, 1125), (1976, 1795)],
+                	refpts = [(100, 115), (100, 310), (100, 500), (530, 115),
+                                                    (530, 310), (530, 500)],
+                	method='lmeds')
 
-
-    # In[161]:
-
-    if "z1000" in filename:
-        img_warped, mat = pcv.transform.warp(img=kept_mask,
-                refimg=nir_img,
-                pts = [(229, 241), (234, 1156), (1050, 1467), (2230, 241),
-                                             (2227, 1171), (1411, 1461)],
-                refpts = [(28, 72), (30, 332), (262,422), (600, 75),
-                                                    (594, 333), (364, 420)],
-                method='lmeds')
-
-        img_warped_original, mat = pcv.transform.warp(img=img,
-                refimg=nir_img,
-                pts = [(229, 241), (234, 1156), (1050, 1467), (2230, 241),
-                                             (2227, 1171), (1411, 1461)],
-                refpts = [(28, 72), (30, 332), (262,422), (600, 75),
-                                                    (594, 333), (364, 420)],
-                method='lmeds')
-
-    elif "z2000" in filename:
-        img_warped, mat = pcv.transform.warp(img=kept_mask,
-            	refimg=nir_img,
-            	pts = [(1005, 1660), (1140, 1730), (1140, 1850), (1413, 1677),
-                                         (1389, 1734), (1389, 1854)],
-            	refpts = [(254, 200), (291, 214), (290, 245), (372, 200),
-                                                (362, 214), (361, 247)],
-            	method='lmeds')
-
-        img_warped_original, mat = pcv.transform.warp(img=img,
-            	refimg=nir_img,
-            	pts = [(1005, 1660), (1140, 1730), (1140, 1850), (1413, 1677),
-                                         (1389, 1734), (1389, 1854)],
-            	refpts = [(254, 200), (291, 214), (290, 245), (372, 200),
-                                                (362, 214), (361, 247)],
-            	method='lmeds')
-
-    else:
-        img_warped, mat = pcv.transform.warp(img=kept_mask,
-            	refimg=nir_img,
-            	pts = [(495, 435), (500, 1120), (510, 1795), (1990, 440),
-                                         (1985, 1125), (1976, 1795)],
-            	refpts = [(100, 115), (100, 310), (100, 500), (530, 115),
-                                                (530, 310), (530, 500)],
-            	method='lmeds')
-
-        img_warped_original, mat = pcv.transform.warp(img=img,
-            	refimg=nir_img,
-            	pts = [(495, 435), (500, 1120), (510, 1795), (1990, 440),
-                                         (1985, 1125), (1976, 1795)],
-            	refpts = [(100, 115), (100, 310), (100, 500), (530, 115),
-                                                (530, 310), (530, 500)],
-            	method='lmeds')
+            img_warped_original, mat = pcv.transform.warp(img=img,
+                	refimg=nir_img,
+                	pts = [(495, 435), (500, 1120), (510, 1795), (1990, 440),
+                                             (1985, 1125), (1976, 1795)],
+                	refpts = [(100, 115), (100, 310), (100, 500), (530, 115),
+                                                    (530, 310), (530, 500)],
+                	method='lmeds')
 
 
-    analysis_nir = pcv.analyze_nir_intensity(nir_img,img_warped, 256, label = "default")
+        analysis_nir = pcv.analyze_nir_intensity(nir_img,img_warped, 256, label = "default")
 
-    # In[164]:
-    #print(pcv.outputs)
+        # In[164]:
+        #print(pcv.outputs)
 
-    #hist_figure1, hist_data1 = pcv.visualize.histogram(nir_img, mask=kept_mask, hist_data=True)
-    wvs1 = [480.0, 550.0, 670.0]
-    wvs2 = [800.0]
-
-
-    fused_img = pcv.image_fusion(img1=img_warped_original, img2=nir_img, wvs1=wvs1, wvs2=wvs2,
-                             array_type="vis-nir_fusion")
+        #hist_figure1, hist_data1 = pcv.visualize.histogram(nir_img, mask=kept_mask, hist_data=True)
+        wvs1 = [480.0, 550.0, 670.0]
+        wvs2 = [800.0]
 
 
-    ndvi = pcv.spectral_index.ndvi(fused_img)
-
-    # Pseudocolor the NDVI image
-    colmap = pcv.visualize.pseudocolor(gray_img=ndvi.array_data, mask=img_warped, cmap="RdYlGn",
-                                       min_value=-0.8, max_value=0.8)
-
-    gdvi = pcv.spectral_index.gdvi(fused_img)
-
-    psri = pcv.spectral_index.psri(fused_img)
+        fused_img = pcv.image_fusion(img1=img_warped_original, img2=nir_img, wvs1=wvs1, wvs2=wvs2,
+                                 array_type="vis-nir_fusion")
 
 
-    colmap = pcv.visualize.pseudocolor(gray_img=psri.array_data, mask=img_warped, cmap="RdYlGn",
-                                       min_value=-0.8, max_value=0.8)
+        ndvi = pcv.spectral_index.ndvi(fused_img)
+
+        # Pseudocolor the NDVI image
+        colmap = pcv.visualize.pseudocolor(gray_img=ndvi.array_data, mask=img_warped, cmap="RdYlGn",
+                                           min_value=-0.8, max_value=0.8)
+
+        gdvi = pcv.spectral_index.gdvi(fused_img)
+
+        psri = pcv.spectral_index.psri(fused_img)
 
 
-    colmap = pcv.visualize.pseudocolor(gray_img=gdvi.array_data, mask=img_warped, cmap="RdYlGn",
-                                       min_value=-0.8, max_value=0.8)
-
-    gdvi_array = np.histogram(gdvi.array_data, bins=256)
+        colmap = pcv.visualize.pseudocolor(gray_img=psri.array_data, mask=img_warped, cmap="RdYlGn",
+                                           min_value=-0.8, max_value=0.8)
 
 
-    # Calculate histogram
-    gdvi_hyper = pcv.hyperspectral.analyze_index(index_array=gdvi, mask=img_warped, bins=100, min_bin=-2, max_bin=2, label="default")
-    psri_hyper = pcv.hyperspectral.analyze_index(index_array=psri, mask=img_warped, bins=100, min_bin=np.amin(psri.array_data), max_bin=np.amax(psri.array_data), label="PSRI array data")
-    ndvi_hyper = pcv.hyperspectral.analyze_index(index_array=ndvi, mask=img_warped, bins=100, min_bin=-1, max_bin=1, label="NDVI array data")
-##    Histogram_for_Hyperspectral(gdvi.array_data, min = -2, max = 2, mask = img_warped)
-##
-##    Histogram_for_Hyperspectral(psri.array_data, min = np.amin(psri.array_data), max = np.amax(psri.array_data), mask = img_warped, label_addition = "PSRI array data")
-##
-##    Histogram_for_Hyperspectral(ndvi.array_data, min = -1, max = 1, mask = img_warped, label_addition = "NDVI array data")
+        colmap = pcv.visualize.pseudocolor(gray_img=gdvi.array_data, mask=img_warped, cmap="RdYlGn",
+                                           min_value=-0.8, max_value=0.8)
 
-    pcv.outputs.save_results(filename=args.result)
+        gdvi_array = np.histogram(gdvi.array_data, bins=256)
+
+
+        # Calculate histogram
+        gdvi_hyper = pcv.hyperspectral.analyze_index(index_array=gdvi, mask=img_warped, bins=100, min_bin=-2, max_bin=2, label="default")
+        psri_hyper = pcv.hyperspectral.analyze_index(index_array=psri, mask=img_warped, bins=100, min_bin=np.amin(psri.array_data), max_bin=np.amax(psri.array_data), label="PSRI array data")
+        ndvi_hyper = pcv.hyperspectral.analyze_index(index_array=ndvi, mask=img_warped, bins=100, min_bin=-1, max_bin=1, label="NDVI array data")
+    ##    Histogram_for_Hyperspectral(gdvi.array_data, min = -2, max = 2, mask = img_warped)
+    ##
+    ##    Histogram_for_Hyperspectral(psri.array_data, min = np.amin(psri.array_data), max = np.amax(psri.array_data), mask = img_warped, label_addition = "PSRI array data")
+    ##
+    ##    Histogram_for_Hyperspectral(ndvi.array_data, min = -1, max = 1, mask = img_warped, label_addition = "NDVI array data")
+
+        pcv.outputs.save_results(filename=args.result)
   #  pcv.outputs.clear()
 
 if __name__=='__main__':
